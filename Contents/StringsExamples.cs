330dd4c04f203b29d8b2cd93635456bdc4ab1ea7
YuriMendess <contato.yurimendess@gmail.com>
2022-11-28 22:07:34 -0300

    :bug: fix: String Examples

--- Contents/StringsExamples.cs
+++ Contents/StringsExamples.cs
@@ -12,95 +12,77 @@ namespace StudiesCSharp.Contents
             #endregion
             
             #region 02 - Interpolação de strings
-            /*
-            static void Main(string[] args)
-            {
                 var price = 12.2;
-                var texto = "O preço do produto é " + price + "apenas a vista!";
-                var texto = string.Format("O preço do produto é {0} apenas a vista! {1}", price, true);
-                var texto = @$"O preço do produto é {price} apenas \n a vista!";
-            }
-            */
+                var text1 = "O preço do produto é " + price + "apenas a vista!";
+                var text2 = string.Format("O preço do produto é {0} apenas a vista! {1}", price, true);
+                var text3 = @$"O preço do produto é {price} apenas \n a vista!";
             #endregion
 
             #region 03 - Comparação de Strings
             
-                var texto = "Testando";
+                var text4 = "Testando";
 
                 // CompareTo => retornará um inteiro
-                Console.WriteLine(texto.CompareTo("Testando")); // 0
-                Console.WriteLine(texto.CompareTo("testando")); // 1
+                Console.WriteLine(text4.CompareTo("Testando")); // 0
+                Console.WriteLine(text4.CompareTo("testando")); // 1
 
                 // Contains() => verifica se a string possui um determinado conjunto de caracteres
-                var texto2 = "Este texto é um teste";
-                Console.WriteLine(texto2.Contains("teste")); // true
-                Console.WriteLine(texto2.Contains("Testando metodo")); // false
+                var text5 = "Este texto é um teste";
+                Console.WriteLine(text5.Contains("teste")); // true
+                Console.WriteLine(text5.Contains("Testando metodo")); // false
 
                 // StringComparison.OrdinalIgnoreCase => ignorará o case sensitive
-                Console.WriteLine(texto2.Contains("TestE", StringComparison.OrdinalIgnoreCase)); // true
+                Console.WriteLine(text5.Contains("TestE", StringComparison.OrdinalIgnoreCase)); // true
             
             #endregion
 
             #region 04 - StartsWith/EndsWith
-            /*
-            static void Main(string[] args)
-            {
-                var texto = "Este texto é um teste";
+                var text6 = "Este texto é um teste";
         
                 // StartsWith
-                Console.WriteLine(texto.StartsWith("Este")); // True
-                Console.WriteLine(texto.StartsWith("este")); // False
-                Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase)); // True
-                Console.WriteLine(texto.StartsWith("texto")); // False
+                Console.WriteLine(text6.StartsWith("Este")); // True
+                Console.WriteLine(text6.StartsWith("este")); // False
+                Console.WriteLine(text6.StartsWith("este", StringComparison.OrdinalIgnoreCase)); // True
+                Console.WriteLine(text6.StartsWith("texto")); // False
         
                 // EndsWith
-                Console.WriteLine($" EndsWith => {texto.EndsWith("teste")}"); // True
-                Console.WriteLine($" EndsWith => {texto.EndsWith("Teste")}"); // False
-                Console.WriteLine($" EndsWith => {texto.EndsWith("Teste", StringComparison.OrdinalIgnoreCase)}"); // True
-                Console.WriteLine($" EndsWith => {texto.EndsWith("um")}"); // False
-            }
-            */
+                Console.WriteLine($" EndsWith => {text6.EndsWith("teste")}"); // True
+                Console.WriteLine($" EndsWith => {text6.EndsWith("Teste")}"); // False
+                Console.WriteLine($" EndsWith => {text6.EndsWith("Teste", StringComparison.OrdinalIgnoreCase)}"); // True
+                Console.WriteLine($" EndsWith => {text6.EndsWith("um")}"); // False
             #endregion
 
             #region 05 - Equals
-            /*
-            static void Main(string[] args)
-            {
-                var texto = "Este texto é um teste";
 
+                var text7 = "Este texto é um teste";
                 // Equals => verifica se a string está exatamente igual
-                Console.WriteLine($" Equals => {texto.Equals("Este texto é um teste")}"); // True
-                Console.WriteLine($" Equals => {texto.Equals("este texto é um teste")}"); // False
-                Console.WriteLine($" Equals => {texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase)}"); // True
-                Console.WriteLine($" Equals => {texto.Equals("teste")}"); // False
-            }
-            */
+                Console.WriteLine($" Equals => {text7.Equals("Este texto é um teste")}"); // True
+                Console.WriteLine($" Equals => {text7.Equals("este texto é um teste")}"); // False
+                Console.WriteLine($" Equals => {text7.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase)}"); // True
+                Console.WriteLine($" Equals => {text7.Equals("teste")}"); // False
             #endregion
 
             #region 06 - Índices
-            /*
-            static void Main(string[] args)
-            {
-                var texto = "este texto é um teste";
+
+                var text8 = "este texto é um teste";
 
                 // IndexOf => o primeiro
-                Console.WriteLine(texto.IndexOf("e")); // 0
-                Console.WriteLine(texto.IndexOf("E")); // -1
+                Console.WriteLine(text8.IndexOf("e")); // 0
+                Console.WriteLine(text8.IndexOf("E")); // -1
 
                 // LastIndexOf => o ultimo
-                Console.WriteLine(texto.LastIndexOf("s")); // 18
-            }
-            */
+                Console.WriteLine(text8.LastIndexOf("s")); // 18
+
             #endregion
             
-            string text = "This is a text variable";
+            string text9 = "This is a text variable";
 
-            Console.WriteLine(text.ToArray());
+            Console.WriteLine(text9.ToArray());
 
             // Trim
-            text.Trim();
-            text.TrimStart();
-            text.TrimEnd();
+            text9.Trim();
+            text9.TrimStart();
+            text9.TrimEnd();
         }
     }
 }
