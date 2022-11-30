4ad0634cba97d3a52bea225f3023022be17911b3
YuriMendess <contato.yurimendess@gmail.com>
2022-11-30 09:12:01 -0300

    :sparkles: feat: Complex Types  Examples

---
+++ ObjectOrientedProgramming/ComplexTypesExamples.cs
@@ -0,0 +1,20 @@
+namespace StudiesCSharp.ObjectOrientedProgramming
+{
+    public class ComplexTypeExamples
+    {
+        public static void ReturnComplexTypesExamples()
+        {
+
+        }
+    }
+
+    class MyAddress
+    {
+        public Address MyStreet { get; set; } 
+    }
+
+    public class Address
+    {
+        private string Street;
+    }
+}
